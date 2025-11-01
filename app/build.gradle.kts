3436feb94b9fc19d178a08e1295b4cf256262e79
maithanhnam <nammai@reecotech.com.vn>
2025-11-01 10:38:26 +0700

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.reecotech.androidtvbox"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
